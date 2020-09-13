818d8c6a42a49b0926d4824fd0a4ee140f7f5a3b
Prasad Koshy Jose <prasadkjose@gmail.com>
2020-09-13 18:30:27 +0200

    added axios API endpoint

--- blog-posts/src/sagas/saga.js
+++ blog-posts/src/sagas/saga.js
@@ -5,6 +5,8 @@ import {
   GET_POSTS_FAILURE_ASYNC,
   GET_POSTS_SUCCESS_ASYNC,
 } from "../reducers/postsReducer";
+import API from "../API/fetchApi";
+
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export const getPostsSaga = () => ({
@@ -20,18 +22,25 @@ export const getPostsFailureSaga = () => ({
   type: GET_POSTS_FAILURE_ASYNC,
 });
 
-//Saga worker
-export function* fetchPostsWorker() {
+function* api() {
   try {
     let data = {};
     let response = {};
     yield put(getPostsSaga());
-    response = yield fetch("https://jsonplaceholder.typicode.com/posts");
-    data = yield response.json();
-    yield put(getPostsSuccessSaga(data));
+    // response = yield fetch("https://jsonplaceholder.typicode.com/posts");
+    // data = yield response.json();
+
+    data = yield API.get("/");
+
+    yield put(getPostsSuccessSaga(data.data));
   } catch (error) {
     yield put(getPostsFailureSaga());
   }
+}
+
+//Saga worker
+export function* fetchPostsWorker() {
+  yield call(api);
 }
 
 export function* fetchPostsWatcher() {
